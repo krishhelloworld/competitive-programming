982dd02da8f9fe8007a0943eb4afe06d6aae20d2
krish gupta <guptakrish2702@gmail.com>
2026-03-24 00:12:07 +0530

    add lets play magic trick and removed empty codes

--- chapter1/coding exercises/brokenKeyboardLIST.cpp
+++ chapter1/coding exercises/brokenKeyboardLIST.cpp
@@ -3,26 +3,26 @@
 using namespace std;
 
 int main() {
-    string line;
+string line;
     while (getline(cin, line)) {
         list<char> text;
         auto cursor = text.begin();
 
-        for (char c : line) {
-            if (c == '[') {
-                cursor = text.begin();
+    for (char c : line) {
+        if (c == '[') {
+        cursor = text.begin();
             }
             else if (c == ']') {
-                cursor = text.end();
+            cursor = text.end();
             }
             else {
-                text.insert(cursor, c);
+            text.insert(cursor, c);
             }
-        }
-
-        for (char c : text)
-            cout << c;
-        cout << '\n';
     }
-    return 0;
+
+for (char c : text)
+cout << c;
+cout << '\n';
+    }
+return 0;
 }
--- chapter1/coding exercises/lets_play_magic.cpp
+++ chapter1/coding exercises/lets_play_magic.cpp
@@ -1,3 +1,12 @@
+//	>cyclic simulation -;
+//		while (true) {
+//		    i = (i + 1) % n;
+//		    if (slot[i] is empty) count++;
+//		}
+//		or 
+//		while(true){
+//			(i>=n) ? i=1 : i++ ;
+// 		}
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
@@ -14,15 +23,8 @@ int main(){
 		arr[i].second= k;
 	}
 
-	for(int i=arr[0].second,j=0; j<n;){
-// 		if(i+arr[j].second<=n)	{
-// 			// i+=arr[j].second;
- // x=i+arr[j].second;
-
-// 		}
-// 		else 					{i+= (arr[j].second-n);}
-		
-		while(count!=arr[j].second){
+	for(int i=1,j=0; j<n;){
+		while(true){
 			if(err[i]=="0") {
 				count++;
 				 if(count==arr[j].second) {
@@ -36,23 +38,3 @@ int main(){
 		cout<<ii<<" ";
 	}	
 }
-// 13
-// AS ACE
-// 2S TWO
-// 3S THREE
-// 4C FOUR
-// 5C FIVE
-// 6C SIX
-// 7D SEVEN
-// 8D EIGHT
-// 9D NINE
-// TH TEN
-// JH JACK
-// QH QUEEN
-// KH KING
-
-// QH 4C AS 8D KH 2S 7D 5C TH JH 3S 6C 9D
-// 9D 4C AS TH 7D 2S QH 5C KH 8D 3S JH 6C 		
-
-
-//if(c[i]==0)count++; count == arr[j].second
